b24ca8518ac8d726d6d6a75d1478636cc9a528c8
Luiz Satto <luiz.satto@retailinmotion.com>
2024-12-10 18:34:10 +0000

    Added CustomExceptionHandler in BuildingBlocks library and configured it in CatalogAPI

--- src/Services/Catalog/Catalog.API/GlobalUsing.cs
+++ src/Services/Catalog/Catalog.API/GlobalUsing.cs
@@ -3,6 +3,8 @@ global using Mapster;
 global using MediatR;
 global using Marten;
 global using FluentValidation;
+global using BuildingBlocks.Behaviors;
 global using BuildingBlocks.CQRS;
+global using BuildingBlocks.Exceptions.Handler;
 global using Catalog.API.Models;
 global using Catalog.API.Exceptions;
