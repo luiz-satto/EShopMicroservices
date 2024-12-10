ee944d0e130f7a3180217a8c62042ac646d75818
Luiz Satto <luiz.satto@retailinmotion.com>
2024-12-10 14:47:39 +0000

    Created GetProductById endpoint for CatalogAPI

--- src/Services/Catalog/Catalog.API/GlobalUsing.cs
+++ src/Services/Catalog/Catalog.API/GlobalUsing.cs
@@ -2,5 +2,6 @@
 global using Mapster;
 global using MediatR;
 global using Marten;
-global using Catalog.API.Models;
 global using BuildingBlocks.CQRS;
+global using Catalog.API.Models;
+global using Catalog.API.Exceptions;
