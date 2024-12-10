3ea992d7cd58993f0389c62e0dd76fd0f4662393
Luiz Satto <luiz.satto@retailinmotion.com>
2024-12-10 17:12:28 +0000

    Added Validation Pipeline Behavior in BuildingBlocks library and implemented CreateProduct Validations

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -10,11 +10,26 @@
 
     public record CreateProductResult(Guid Id);
 
-    internal class CreateProductHandler(IDocumentSession session)
-        : ICommandHandler<CreateProductCommand, CreateProductResult>
+    public class CreateProductValidation : AbstractValidator<CreateProductCommand>
+    {
+        public CreateProductValidation()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required");
+            RuleFor(x => x.Category).NotEmpty().WithMessage("Category is required");
+            RuleFor(x => x.ImageFile).NotEmpty().WithMessage("ImageFile is required");
+            RuleFor(x => x.Price).GreaterThan(0).WithMessage("Price must be greater than 0");
+        }
+    }
+
+    internal class CreateProductHandler(
+        IDocumentSession session,
+        ILogger<CreateProductHandler> logger
+    ) : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
         public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
         {
+            logger.LogInformation("CreateProductHandler.Handle called with {@Command}", command);
+
             var product = new Product
             {
                 Name = command.Name,
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,14 +1,21 @@
+using BuildingBlocks.Behaviors;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddCarter();
+var assembly = typeof(Program).Assembly;
 
 builder.Services.AddMediatR(config =>
 {
-    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+    config.RegisterServicesFromAssembly(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
 });
 
-builder.Services.AddMarten(options => 
+builder.Services.AddValidatorsFromAssembly(assembly);
+
+builder.Services.AddCarter();
+
+builder.Services.AddMarten(options =>
 {
     options.Connection(builder.Configuration.GetConnectionString("Database")!);
 }).UseLightweightSessions();
