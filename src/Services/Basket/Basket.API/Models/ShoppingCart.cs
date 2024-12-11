b47865c23009e081a4ad85bbd7686f62ec0bea3b
Luiz Satto <luiz.satto@retailinmotion.com>
2024-12-11 14:41:02 +0000

    Created Basket.API microservice and Domain Entities

---
+++ src/Services/Basket/Basket.API/Models/ShoppingCart.cs
@@ -0,0 +1,20 @@
+﻿namespace Basket.API.Models
+{
+    public class ShoppingCart
+    {
+        public string UserName { get; set; } = default!;
+        public List<ShoppingCartItem> Items { get; set; } = [];
+        public decimal TotalPrice => Items.Sum(x => x.Price * x.Quantity);
+
+        // Required for mapping
+        public ShoppingCart()
+        {
+
+        }
+
+        public ShoppingCart(string userName)
+        {
+            UserName = userName;
+        }
+    }
+}
