13391999660d718f444a350ccae09cb4cbff8ac3
luiz-satto <luizf.satto@gmail.com>
2024-12-14 19:40:36 +0000

    Configured Seed and Auto-Migrate SQLite Db using EFCore in Discount.GRPC

--- src/Services/Discount/Discount.GRPC/Data/DiscountContext.cs
+++ src/Services/Discount/Discount.GRPC/Data/DiscountContext.cs
@@ -6,5 +6,13 @@ namespace Discount.GRPC.Data
     public class DiscountContext(DbContextOptions<DiscountContext> options) : DbContext(options)
     {
         public DbSet<Coupon> Coupons { get; set; } = default!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Coupon>().HasData(
+                new Coupon() { Id = 1, ProductName = "iPhone X", Description = "iPhone X - 10% Discount!", Amount = 10 },
+                new Coupon() { Id = 2, ProductName = "Samsung 10", Description = "Samsung 10 - 20% Discount!", Amount = 10 }
+            );
+        }
     }
 }
