07a88d7a1abed5ae44699d9d40ff7b9e6b6206cc
luiz-satto <luizf.satto@gmail.com>
2024-12-14 18:45:48 +0000

    Configured and Registred SQLite Db in Discount.GRPC

---
+++ src/Services/Discount/Discount.GRPC/Data/DiscountContext.cs
@@ -0,0 +1,10 @@
+﻿using Discount.GRPC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Discount.GRPC.Data
+{
+    public class DiscountContext(DbContextOptions<DiscountContext> options) : DbContext(options)
+    {
+        public DbSet<Coupon> Coupons { get; set; } = default!;
+    }
+}
--- src/Services/Discount/Discount.GRPC/Program.cs
+++ src/Services/Discount/Discount.GRPC/Program.cs
@@ -1,12 +1,21 @@
+using Discount.GRPC.Data;
+using Discount.GRPC.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+#region Add services to the container
 builder.Services.AddGrpc();
+builder.Services.AddDbContext<DiscountContext>(options => {
+    options.UseSqlite(builder.Configuration.GetConnectionString("Database"));
+});
+#endregion
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-//app.MapGrpcService<GreeterService>();
+#region Configure the HTTP request pipeline
+app.MapGrpcService<DiscountService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+#endregion
 
 app.Run();
