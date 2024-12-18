acf58bfe28621288e16a81475fe9a5e9b8f89259
Luiz Satto <luiz.satto@retailinmotion.com>
2024-12-18 11:48:46 +0000

    Added DependencyInjection between clean architecture layers

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,9 +1,19 @@
+using Ordering.API;
+using Ordering.Application;
+using Ordering.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 #region Add services to the container
+builder.Services
+    .AddApplicationServices()
+    .AddInfrastructureServices(builder.Configuration)
+    .AddApiServices();
+
 var app = builder.Build();
 #endregion
 
 #region Configure the HTTP request pipeline
+app.UseApiServices();
 app.Run();
 #endregion
