23322b944a78ba1afc0f9ea63ff3456dfa61a458
luiz-satto <luizf.satto@gmail.com>
2024-12-20 12:03:40 +0000

    Added a Rich Domain Model for Order Entities in Ordering.API

--- src/Services/Ordering/Ordering.Domain/ValueObjects/OrderName.cs
+++ src/Services/Ordering/Ordering.Domain/ValueObjects/OrderName.cs
@@ -2,6 +2,14 @@
 {
     public record OrderName
     {
+        private const int DefaultLength = 5;
         public string Value { get; } = default!;
+        private OrderName(string value) => Value = value;
+        public static OrderName Of(string value)
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(value);
+            ArgumentOutOfRangeException.ThrowIfNotEqual(value.Length, DefaultLength);
+            return new OrderName(value);
+        }
     }
 }
